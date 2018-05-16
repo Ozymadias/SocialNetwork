f7d81109563633f90ff4b1e869a3173abf5a72dc
Ozymadias <saidamyzo@gmail.com>
2018-05-16 13:04:20 +0200

    Fix bug with wrong names of field in queries

--- src/main/java/com/socialnet/repository/NodeRepository.java
+++ src/main/java/com/socialnet/repository/NodeRepository.java
@@ -10,18 +10,18 @@ import java.util.Collection;
 public interface NodeRepository extends Neo4jRepository<Node, Long> {
     Node findByMongoId(String name);
 
-    @Query("MATCH (p1{name: {firstName}})-[r:IS_FRIEND]-(p2{name: {secondName}}) DELETE r")
-    void unfriend(@Param("firstName") String firstName, @Param("secondName") String secondName);
+    @Query("MATCH (p1{mongoId: {firstId}})-[r:IS_FRIEND]-(p2{mongoId: {secondId}}) DELETE r")
+    void unfriend(@Param("firstId") String firstId, @Param("secondId") String secondId);
 
-    @Query("MATCH (p1)-[r:INVITATION]->(p2{name: {userId}}) RETURN p1")
+    @Query("MATCH (p1)-[r:INVITATION]->(p2{mongoId: {userId}}) RETURN p1")
     Collection<Node> invitations(@Param("userId") String userId);
 
-    @Query("MATCH (p1)-[r:IS_FRIEND]-(p2{name: {userId}}) RETURN p1")
+    @Query("MATCH (p1)-[r:IS_FRIEND]-(p2{mongoId: {userId}}) RETURN p1")
     Collection<Node> friends(@Param("userId") String userId);
 
-    @Query("MATCH (p1{name: {userId}})-[r:INVITATION]->(p2{name: {inviteeId}}) DELETE r")
+    @Query("MATCH (p1{mongoId: {userId}})-[r:INVITATION]-(p2{mongoId: {inviteeId}}) DELETE r")
     void refuseInvitation(@Param("userId") String userId, @Param("inviteeId") String inviteeId);
 
-    @Query("MATCH (p1)-[r:IS_FRIEND*]-(p2{name: {userId}}) RETURN p1")
+    @Query("MATCH (p1)-[r:IS_FRIEND*]-(p2{mongoId: {userId}}) RETURN p1")
     Collection<Node> network(@Param("userId") String userId);
 }
