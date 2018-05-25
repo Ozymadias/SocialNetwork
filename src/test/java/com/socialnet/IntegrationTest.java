45c76def105bcf8c1af5a2e1ab42be79a2965e68
Ozymadias <saidamyzo@gmail.com>
2018-05-25 16:50:20 +0200

    Clean up IntegrationTest class and test interaction with neo4j database

--- src/test/java/com/socialnet/IntegrationTest.java
+++ src/test/java/com/socialnet/IntegrationTest.java
@@ -1,11 +1,11 @@
 package com.socialnet;
 
+import com.socialnet.pojos.Node;
 import com.socialnet.pojos.User;
+import com.socialnet.repositories.NodeRepository;
 import com.socialnet.repositories.UserRepository;
-import com.socialnet.routes.*;
-import io.restassured.RestAssured;
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.test.junit4.CamelTestSupport;
+import io.restassured.response.Response;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,29 +13,33 @@ import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.testng.annotations.BeforeTest;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
+import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 @ContextConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class IntegrationTest extends CamelTestSupport {
+public class IntegrationTest {
     @LocalServerPort
     private int port;
 
     @Autowired
     private UserRepository userRepository;
 
-    @BeforeTest
+    @Autowired
+    private NodeRepository nodeRepository;
+
+    @Before
     public void setUp() {
-//        RestAssured.baseURI = "http://localhost";
         userRepository.deleteAll();
+        nodeRepository.deleteAll();
     }
 
     @Test
@@ -56,13 +60,12 @@ public class IntegrationTest extends CamelTestSupport {
         String birthDate = "0-2-2";
         given().port(port).when().post("/register?name=" + name + "&city=" + city + "&birthDate=" + birthDate).then().statusCode(200);
 
-        given().port(port).when().get("/findAll").then().statusCode(200);
-        User[] users = given().port(port).when().get("/findAll").as(User[].class);
-        assertThat(users.length, is(1));
-        User user = users[0];
-        assertThat(user.getName(), is(name));
-        assertThat(user.getCity(), is(city));
-        assertThat(user.getBirthDate(), is(birthDate));
+        Response response = given().port(port).when().get("/findAll");
+
+        response.then().statusCode(200);
+        response.then().body("name", contains(name));
+        response.then().body("city", contains(city));
+        response.then().body("birthDate", contains(birthDate));
     }
 
     @Test
@@ -96,7 +99,6 @@ public class IntegrationTest extends CamelTestSupport {
         User userToSave = new User(name, city, birthDate);
         userRepository.save(userToSave);
 
-        given().port(port).when().get("/findAll").then().statusCode(200);
         User[] users = given().port(port).when().get("/findAll").as(User[].class);
         assertThat(users.length, is(1));
         assertThat(users[0], is(userToSave));
@@ -110,13 +112,13 @@ public class IntegrationTest extends CamelTestSupport {
         User userToSave = new User(name, city, birthDate);
         userRepository.save(userToSave);
 
-        String path = "/findByCity?city=" + city;
-        given().port(port).when().get(path).then().statusCode(200);
-        User[] users = given().port(port).when().get(path).as(User[].class);
+        Response response = given().port(port).when().get("/findByCity?city=" + city);
+
+        response.then().statusCode(200);
+        User[] users = response.as(User[].class);
         assertThat(users.length, is(1));
         assertThat(users[0], is(userToSave));
     }
-
 
     @Test
     public void findByCityShouldContainsOneUserAfterInsertingTwo() {
@@ -131,9 +133,10 @@ public class IntegrationTest extends CamelTestSupport {
         userRepository.save(secondUserToSave);
         userRepository.save(userToSave);
 
-        String path = "/findByCity?city=" + city;
-        given().port(port).when().get(path).then().statusCode(200);
-        User[] users = given().port(port).when().get(path).as(User[].class);
+        Response response = given().port(port).when().get("/findByCity?city=" + city);
+
+        response.then().statusCode(200);
+        User[] users = response.as(User[].class);
         assertThat(users.length, is(1));
         assertThat(Arrays.asList(users), contains(userToSave));
     }
@@ -152,14 +155,39 @@ public class IntegrationTest extends CamelTestSupport {
         userRepository.save(secondUserToSave);
         userRepository.save(userToSave);
 
-        given().port(port).when().get("/findAll").then().statusCode(200);
-        User[] users = given().port(port).when().get("/findAll").as(User[].class);
+        Response response = given().port(port).when().get("/findAll");
+
+        response.then().statusCode(200);
+        User[] users = response.as(User[].class);
         assertThat(users.length, is(2));
         assertThat(Arrays.asList(users), containsInAnyOrder(usersToSave));
     }
 
-    @Override
-    protected RouteBuilder createRouteBuilder() {
-        return new ExternalRouteBuilder();
+    @Test
+    public void neo4jDatabaseShouldContainsUserAfterRegistration() {
+        String name = "Name";
+        String city = "City";
+        String birthDate = "0-1-1";
+
+        Response response = given().port(port).when().post("/register?name=" + name + "&city=" + city + "&birthDate=" + birthDate);
+        String mongoId = response.getHeader("mongoId");
+        Iterable<Node> all = nodeRepository.findAll();
+
+        assertTrue(all.iterator().hasNext());
+        assertThat(all.iterator().next().getMongoId(), is(mongoId));
+    }
+
+    @Test
+    public void neo4jDatabaseShouldContainsInvitation() {
+        String name = "Name";
+        String city = "City";
+        String birthDate = "0-1-1";
+
+        Response response = given().port(port).when().post("/register?name=" + name + "&city=" + city + "&birthDate=" + birthDate);
+        String mongoId = response.getHeader("mongoId");
+        Response secondResponse = given().port(port).when().post("/register?name=" + name + "&city=" + city + "&birthDate=" + birthDate);
+        String secondMongoId = secondResponse.getHeader("mongoId");
+        given().port(port).when().post("/" + secondMongoId + "/invite?inviteeId=" + mongoId);
+        given().port(port).when().get("/" + mongoId + "/invitations").then().body("mongoId", contains(secondMongoId));
     }
 }
